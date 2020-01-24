c6e8aba07cb3ac0b70c483afe3ef645ca940839a
leobaz <leutrim.kosumi@hotmail.com>
2020-01-24 16:26:19 +0100

    new offers

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -10,6 +11,8 @@ import { LoadingController } from '@ionic/angular';
 })
 export class AuthPage implements OnInit {
   isLoading = false;
+  isLogin = true;
+
   constructor(private authService: AuthService, private router: Router, private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
@@ -26,7 +29,25 @@ export class AuthPage implements OnInit {
       }, 1500);
     });
     this.authService.login();
-    
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password  = form.value.password;
+    console.log(email, password);
+
+    if (this.isLogin) {
+
+    } else {
+
+    }
+  }
+
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
   }
 
 }
